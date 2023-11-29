068fa5ea2eafe1542a99bd6f803d3bd8b51d3051
Leoesbravo <lescogidobravo@outlook.com>
2023-11-29 13:18:27 -0600

    Connection

--- DL/ControlEntrevistaContext.cs
+++ DL/ControlEntrevistaContext.cs
@@ -33,7 +33,7 @@ public partial class ControlEntrevistaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.; Database=GeneradorCitas; Trusted_Connection=True; TrustServerCertificate=True; User ID=sa; Password=pass@word1; Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Data Source=192.168.0.152;Initial Catalog=GeneradorCitas;User ID=sa;Password=Qwerty123456$$$#;Trust Server Certificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
