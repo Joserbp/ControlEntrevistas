1c364cf760605bd04eaef112ca6db9413dc2ed1e
Joserbp <josera.jack@gmail.com>
2023-11-29 12:37:31 -0600

    Form de citas con Candidato detalle DDL de Vacantes

---
+++ BL/Vacante.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Vacante
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.ControlEntrevistaContext context = new DL.ControlEntrevistaContext())
+                {
+                    var vacantes = context.Candidatos.FromSqlRaw("VacanteGetAll").ToList();
+                    if (vacantes.Count > 0)
+                    {
+                        result.Objects = new List<object>();
+                        foreach (var objVacante in vacantes)
+                        {
+                            ML.Vacante vacante = new ML.Vacante
+                            {
+                                IdVacante = objVacante.IdVacante.Value,
+                                Nombre = objVacante.Nombre,
+                            };
+                            result.Objects.Add(vacante);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
