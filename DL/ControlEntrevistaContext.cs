05f6e597665cd7d569d9dd072065ada62ef17052
Leoesbravo <lescogidobravo@outlook.com>
2023-12-07 11:30:20 -0600

    com

--- DL/ControlEntrevistaContext.cs
+++ DL/ControlEntrevistaContext.cs
@@ -32,7 +32,6 @@ public partial class ControlEntrevistaContext : DbContext
     public virtual DbSet<Vacante> Vacantes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=192.168.0.152;Initial Catalog=GeneradorCitas;User ID=sa;Password=Qwerty123456$$$#;Trust Server Certificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
